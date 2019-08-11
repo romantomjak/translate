06df7fb6e2520be6f5d7b093f97216ea7bf9572d
Roman Tomjak <6570684+romantomjak@users.noreply.github.com>
2019-08-11 13:37:14 +0100

    add tests for main cli args

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"os"
 	"strings"
 )
@@ -26,11 +27,11 @@ Options:
 
   -key=<key>
     The secret key to use to authenticate API requests with.
-	Overrides the RT_TRANSLATE_KEY environment variable if set.
+	Overrides the TRANSLATE_KEY environment variable if set.
 
   -to=<language>
     The ISO-639-1 language code to use for translation of the input text.
-	Overrides the RT_TRANSLATE_TO environment variable if set.
+	Overrides the TRANSLATE_TO environment variable if set.
 
   -from=<language>
 	The ISO-639-1 language code of the source text. If the source
@@ -40,31 +41,46 @@ Options:
 )
 
 func main() {
+	os.Exit(Run(os.Stdin, os.Stdout, os.Stdout, os.Args[1:]))
+}
+
+func Run(stdin io.Reader, stdout, stderr io.Writer, args []string) int {
 	var fromLang, toLang, apiKey string
 
-	flag.StringVar(&fromLang, "from", "", "source language")
-	flag.StringVar(&toLang, "to", "", "target language")
-	flag.StringVar(&apiKey, "key", "", "secret key")
-	flag.Usage = func() {
-		fmt.Fprint(os.Stderr, strings.TrimSpace(usage))
+	flags := flag.NewFlagSet("", flag.ContinueOnError)
+	flags.StringVar(&fromLang, "from", "", "source language")
+	flags.StringVar(&toLang, "to", "", "target language")
+	flags.StringVar(&apiKey, "key", "", "secret key")
+	flags.Usage = func() {
+		fmt.Fprint(stderr, strings.TrimSpace(usage))
 	}
-	flag.Parse()
 
-	text := flag.Args()
-
-	if toLang == "" {
-		fmt.Fprintln(os.Stderr, "error: empty target language code")
-		return
+	if err := flags.Parse(args); err != nil {
+		return 1
 	}
 
 	if apiKey == "" {
-		fmt.Fprintln(os.Stderr, "error: empty secret key")
-		return
+		apiKey = os.Getenv("TRANSLATE_KEY")
 	}
 
+	if apiKey == "" {
+		fmt.Fprintln(stderr, "error: empty secret key")
+		return 1
+	}
+
+	if toLang == "" {
+		toLang = os.Getenv("TRANSLATE_TO")
+	}
+
+	if toLang == "" {
+		fmt.Fprintln(stderr, "error: empty target language code")
+		return 1
+	}
+
+	text := flag.Args()
 	if len(text) == 0 {
-		fmt.Fprintln(os.Stderr, "error: nothing to translate")
-		return
+		fmt.Fprintln(stderr, "error: nothing to translate")
+		return 1
 	}
 
 	client := NewClient(apiKey)
@@ -72,11 +88,13 @@ func main() {
 
 	translations, err := translator.Translate(fromLang, toLang, text)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "error: %v\n", err)
-		return
+		fmt.Fprintf(stderr, "error: %v\n", err)
+		return 1
 	}
 
 	for _, translation := range translations {
-		fmt.Fprintf(os.Stdout, "%s\n", translation.TranslatedText)
+		fmt.Fprintf(stdout, "%s\n", translation.TranslatedText)
 	}
+
+	return 0
 }
--- main_test.go
+++ main_test.go
@@ -1,4 +1,61 @@
 package main
 
-// add test for env vars
-// add test for cli flag overrides
+import (
+	"bytes"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func assertEqual(t *testing.T, got, want interface{}) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Fatalf("got %+v, want %+v", got, want)
+	}
+}
+
+func assertContains(t *testing.T, got, want string) {
+	t.Helper()
+	if !strings.Contains(got, want) {
+		t.Fatalf("expected %q to contain %q, but it didn't", got, want)
+	}
+}
+
+func TestRun_ApiKeyNotSet(t *testing.T) {
+	stdin := new(bytes.Buffer)
+	stdout := new(bytes.Buffer)
+	stderr := new(bytes.Buffer)
+	args := []string{}
+
+	code := Run(stdin, stdout, stderr, args)
+	assertEqual(t, code, 1)
+
+	out := stderr.String()
+	assertContains(t, out, "empty secret key")
+}
+
+func TestRun_TargetLanguageNotSet(t *testing.T) {
+	stdin := new(bytes.Buffer)
+	stdout := new(bytes.Buffer)
+	stderr := new(bytes.Buffer)
+	args := []string{"-key=test"}
+
+	code := Run(stdin, stdout, stderr, args)
+	assertEqual(t, code, 1)
+
+	out := stderr.String()
+	assertContains(t, out, "empty target language code")
+}
+
+func TestRun_TextNotSet(t *testing.T) {
+	stdin := new(bytes.Buffer)
+	stdout := new(bytes.Buffer)
+	stderr := new(bytes.Buffer)
+	args := []string{"-key=test", "-to=en"}
+
+	code := Run(stdin, stdout, stderr, args)
+	assertEqual(t, code, 1)
+
+	out := stderr.String()
+	assertContains(t, out, "nothing to translate")
+}
